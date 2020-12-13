510bb4ed4bcca473aadb65d435ab58a3759df3ca
Emily Thomas <ethomas@quint.co.uk>
2020-12-12 22:45:27 -0800

    added functions to complete rendering and team html

--- app.js
+++ app.js
@@ -10,26 +10,101 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeCount = [{
+    type: 'input',
+    message: 'How many employees do you have?',
+    name: 'employeeCount'
+},
+]
+const managerQuestions = [{
+    type: 'input',
+    message: 'What is your office number?',
+    name: 'officeNumber'
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+},
+]
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+const employeeQuestions = [{
+    type: 'input',
+    message: 'What is your role?',
+    name: 'role'
+},
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+{
+    type: 'input',
+    message: 'What is your name?',
+    name: 'name'
+},
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+{
+    type: 'input',
+    message: 'What is your id?',
+    name: 'id'
+},
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+{
+    type: 'input',
+    message: 'What is your email?',
+    name: 'email'
+},
+]
+
+const internQuestions = [{
+    type: 'input',
+    message: 'What is the name of your school? ',
+    name: 'school'
+},
+]
+
+const engineerQuestions = [{
+    type: 'input',
+    message: 'What is the name of your github username? ',
+    name: 'github'
+},
+]
+var employeeData = []
+inquirer
+        .prompt(employeeCount)
+        .then(async function (data) {
+            for (let i = 0; i < data.employeeCount; i++) {
+                await inquirer
+                .prompt(employeeQuestions)
+                .then(async function (employee){
+                    if (employee.role.toLowerCase() === "manager") {
+                        await inquirer
+                        .prompt(managerQuestions)
+                        .then(function (officeNumber){
+                            employeeData .push (new Manager (employee.name, employee.id, employee.email, officeNumber.officeNumber)) 
+                        })
+                    } else if (employee.role.toLowerCase()=== "engineer") {     
+                        await inquirer
+                        .prompt(engineerQuestions)
+                        .then(function (gitHub){
+                            employeeData .push (new Engineer (employee.name, employee.id, employee.email, gitHub.github))
+                        })
+                    } else if (employee.role.toLowerCase() === "intern"){
+                        await inquirer
+                        .prompt(internQuestions)
+                        .then(function (school){
+                            employeeData .push (new Intern (
+                                employee.name, employee.id, employee.email, school.school
+                            ))
+                        })
+                        
+                    }
+                })
+
+            }
+            writeToFile (render(employeeData))
+        })
+
+        async function writeToFile (employeeHtml) {
+            if (!fs.existsSync(OUTPUT_DIR)) {
+                await fs.promises.mkdir(OUTPUT_DIR).catch(console.error);
+            }
+                fs.writeFile(outputPath, employeeHtml, (err) => {
+                    if (err) throw err;
+                });
+        }
+
+
